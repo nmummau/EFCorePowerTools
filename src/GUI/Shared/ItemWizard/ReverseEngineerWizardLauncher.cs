ec96585122d05b99a49084c3ae9e3e4f4a843377
Neal Mummau <nealmummau@gmail.com>
2026-04-14 17:13:25 -0400

    fix vs

--- src/GUI/Shared/ItemWizard/ReverseEngineerWizardLauncher.cs
+++ src/GUI/Shared/ItemWizard/ReverseEngineerWizardLauncher.cs
@@ -2,18 +2,17 @@ using System.Collections.Generic;
 using EFCorePowerTools.Handlers.ReverseEngineer;
 using EnvDTE;
 using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.TemplateWizard;
 
 namespace EFCorePowerTools.ItemWizard
 {
-    public class ReverseEngineerWizardLauncher : IWizard
+    public class ReverseEngineerWizardLauncher : Microsoft.VisualStudio.TemplateWizard.IWizard
     {
         // Update vstemplate version reference if main assembly version changes
         // In \src\GUI\WizardItemTemplate\WizardItemTemplate.vstemplate
-        public void RunStarted(
+        void Microsoft.VisualStudio.TemplateWizard.IWizard.RunStarted(
             object automationObject,
             Dictionary<string, string> replacementsDictionary,
-            WizardRunKind runKind,
+            Microsoft.VisualStudio.TemplateWizard.WizardRunKind runKind,
             object[] customParams)
         {
             if (PackageManager.Package != null)
@@ -25,32 +24,32 @@ namespace EFCorePowerTools.ItemWizard
 
         // This method is only called for item templates,
         // not for project templates.
-        public bool ShouldAddProjectItem(string filePath)
+        bool Microsoft.VisualStudio.TemplateWizard.IWizard.ShouldAddProjectItem(string filePath)
         {
             return false;
         }
 
         // This method is called after the project is created.
-        public void RunFinished()
+        void Microsoft.VisualStudio.TemplateWizard.IWizard.RunFinished()
         {
             // Ignore
         }
 
         // This method is only called for item templates,
         // not for project templates.
-        public void ProjectItemFinishedGenerating(ProjectItem projectItem)
+        void Microsoft.VisualStudio.TemplateWizard.IWizard.ProjectItemFinishedGenerating(ProjectItem projectItem)
         {
             // Never called due to false above
         }
 
         // This method is called before opening any item that
         // has the OpenInEditor attribute.
-        public void BeforeOpeningFile(ProjectItem projectItem)
+        void Microsoft.VisualStudio.TemplateWizard.IWizard.BeforeOpeningFile(ProjectItem projectItem)
         {
             // Never called, as this is diabled
         }
 
-        public void ProjectFinishedGenerating(Project project)
+        void Microsoft.VisualStudio.TemplateWizard.IWizard.ProjectFinishedGenerating(Project project)
         {
             // Never called, this is an item template
         }
